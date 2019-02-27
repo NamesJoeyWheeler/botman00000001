213731c517c00712506ad55aad5aa7fc1a5d7e04
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2019-02-27 19:42:17 +1100

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,5 @@ client.on('message', msg => {
     msg.reply('Hello there.');
   }
 });
-// add message as a parameter to your callback function
-bot.on('message', function(message) {
-    // Now, you can use the message variable inside
-    if (message.content === "$loop") { 
-        var interval = setInterval (function () {
-            // use the message's channel (TextChannel) to send a new message
-            message.channel.send("123")
-            .catch(console.error); // add error handling here
-        }, 1 * 1000); 
-    }
-});
 
 client.login(process.env.BOT_TOKEN);
